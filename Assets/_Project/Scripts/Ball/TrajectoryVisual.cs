4d6a27a2121e3286eadcab5a46fb90620addd920
Bohdan Tsaplia <b.tsaplia@gmail.com>
2023-03-16 17:44:30 +0200

    add trajectory visual

---
+++ Assets/_Project/Scripts/Ball/TrajectoryVisual.cs
@@ -0,0 +1,62 @@
+﻿using Gisha.BallGame.Core;
+using UnityEngine;
+
+namespace Gisha.BallGame.Ball
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class TrajectoryVisual : MonoBehaviour
+    {
+        [SerializeField] private float maxLength = 5f;
+
+        private LineRenderer _lineRenderer;
+        private IWorldTouchController _touchController;
+
+        private void Awake()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+        }
+
+        private void Start()
+        {
+            _touchController = GameManager.Instance.WorldTouchController;
+
+            _touchController.WorldTouchDown += OnTouchDown;
+            _touchController.WorldTouchUp += OnTouchUp;
+        }
+
+        private void OnDisable()
+        {
+            _touchController.WorldTouchDown -= OnTouchDown;
+            _touchController.WorldTouchUp -= OnTouchUp;
+        }
+
+        private void OnTouchUp(Vector3 pos)
+        {
+            _lineRenderer.enabled = false;
+        }
+
+        private void OnTouchDown(Vector3 pos)
+        {
+            _lineRenderer.enabled = true;
+        }
+
+        private void Update()
+        {
+            if (!_touchController.IsFingerDown)
+                return;
+
+            BuildTrajectoryLine();
+        }
+
+        private void BuildTrajectoryLine()
+        {
+            var endPos = _touchController.FingerWorldPosition - transform.position;
+            endPos.y = 0f;
+
+            if (endPos.magnitude > maxLength)
+                endPos = Vector3.ClampMagnitude(endPos, maxLength);
+
+            _lineRenderer.SetPosition(1, endPos);
+        }
+    }
+}
